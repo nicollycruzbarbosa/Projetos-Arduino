7105fae38b7dbebbe2e59fab02d975c7101dfa13
Nicolly Cruz Barbosa <nicollycruzbar@gmail.com>
2024-08-05 15:22:38 -0300

    PiscarLed/

--- PiscarLed/PiscarLed.ino
+++ PiscarLed/PiscarLed.ino
@@ -1,3 +1,5 @@
+//programa que envia sinalalto para a saída 7,
+//aguarda 1 segundo e envia sinal baixo para a saída 7,
 void setup() {
   // put your setup code here, to run once:
   pinMode(7,OUTPUT);
@@ -5,8 +7,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+//  envia sinal alto
 digitalWrite(7,HIGH);
 delay(1000);
+//envia sinal baixo
 digitalWrite(7, LOW);
 delay(1000);
 }
