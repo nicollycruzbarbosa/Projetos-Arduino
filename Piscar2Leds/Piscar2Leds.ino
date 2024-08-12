567202f79c3161d37bd745086ab5649a1666e3d7
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2024-08-12 08:59:18 -0300

    Create Piscar2Leds.ino

---
+++ Piscar2Leds/Piscar2Leds.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  /programa que envia sinalalto para a saída 7,
+//aguarda 1 segundo e envia sinal baixo para a saída 7,
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(7,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  envia sinal alto
+digitalWrite(7,HIGH);//  envia sinal alto
+delay(1000);
+digitalWrite(7, LOW);//envia sinal baixo
+delay(1000);
+digitalWrite(8,HIGH);//  envia sinal alto
+delay(1000);
+digitalWrite(8, LOW);//envia sinal baixo
+delay(1000);
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
